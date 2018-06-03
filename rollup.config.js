6f4ca45c39b427bcf6cb10c20c5c973e84cd7d91
Matt Travi <programmer@travi.org>
2018-06-02 23:28:16 -0500

    build(rollup): marked external dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@
 
 export default {
   input: 'src/index.js',
+  external: ['util', 'write-yaml'],
   output: [
     {file: 'lib/index.cjs.js', format: 'cjs', sourcemap: true},
     {file: 'lib/index.es.js', format: 'es', sourcemap: true}
