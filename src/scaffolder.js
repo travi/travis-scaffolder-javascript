06e655b20cd716bd0af0a8bad94f31f89e5e4b7c
Matt Travi <programmer@travi.org>
2018-06-06 00:00:51 -0500

    feat(publish-package): added semantic-release as a deploy step
    for travi/javascript-scaffolder#32 and travi/javascript-scaffolder#5

--- src/scaffolder.js
+++ src/scaffolder.js
@@ -1,13 +1,15 @@
 import writeYaml from '../third-party-wrappers/write-yaml';
 
-export default async function ({projectRoot, vcs, visibility}) {
+export default async function ({projectRoot, vcs, visibility, packageType}) {
   await writeYaml(`${projectRoot}/.travis.yml`, {
     language: 'node_js',
     notifications: {email: false},
+    ...'Package' === packageType && {branches: {except: ["/^v\\d+\\.\\d+\\.\\d+$/"]}},
     install: ['npm install', 'gem install travis'],
     before_script: ['npm run greenkeeper:update-lockfile', 'npm ls >/dev/null'],
     after_script: 'npm run greenkeeper:upload-lockfile',
     ...'Public' === visibility && {after_success: 'npm run coverage:report'},
+    ...'Package' === packageType && {deploy: {provider: 'script', skip_cleanup: true, script: 'npx semantic-release'}},
     env: {global: ['FORCE_COLOR=1', 'NPM_CONFIG_COLOR=always', 'GK_LOCK_COMMIT_AMEND=true']}
   });
 
--- test/unit/scaffolder-test.js
+++ test/unit/scaffolder-test.js
@@ -13,36 +13,34 @@ suite('travis', () => {
     sandbox = sinon.createSandbox();
 
     sandbox.stub(yamlWriter, 'default');
+
+    yamlWriter.default.resolves();
   });
 
   teardown(() => sandbox.restore());
 
-  test('that a base config is created for a javascript project', () => {
-    yamlWriter.default.resolves();
-
-    return assert.becomes(
-      scaffold({projectType: 'JavaScript', projectRoot, vcs, visibility: 'Public'}),
-      {
-        badge: {
-          img: `https://img.shields.io/travis/${vcs.owner}/${vcs.name}.svg?branch=master`,
-          link: `https://travis-ci.com/${vcs.owner}/${vcs.name}`,
-          text: 'Build Status'
-        }
+  test('that a base config is created for a javascript project', () => assert.becomes(
+    scaffold({projectType: 'JavaScript', projectRoot, vcs, visibility: 'Public'}),
+    {
+      badge: {
+        img: `https://img.shields.io/travis/${vcs.owner}/${vcs.name}.svg?branch=master`,
+        link: `https://travis-ci.com/${vcs.owner}/${vcs.name}`,
+        text: 'Build Status'
       }
-    ).then(() => assert.calledWith(
-      yamlWriter.default,
-      `${projectRoot}/.travis.yml`,
-      {
-        language: 'node_js',
-        notifications: {email: false},
-        install: ['npm install', 'gem install travis'],
-        before_script: ['npm run greenkeeper:update-lockfile', 'npm ls >/dev/null'],
-        after_script: 'npm run greenkeeper:upload-lockfile',
-        after_success: 'npm run coverage:report',
-        env: {global: ['FORCE_COLOR=1', 'NPM_CONFIG_COLOR=always', 'GK_LOCK_COMMIT_AMEND=true']}
-      }
-    ));
-  });
+    }
+  ).then(() => assert.calledWith(
+    yamlWriter.default,
+    `${projectRoot}/.travis.yml`,
+    {
+      language: 'node_js',
+      notifications: {email: false},
+      install: ['npm install', 'gem install travis'],
+      before_script: ['npm run greenkeeper:update-lockfile', 'npm ls >/dev/null'],
+      after_script: 'npm run greenkeeper:upload-lockfile',
+      after_success: 'npm run coverage:report',
+      env: {global: ['FORCE_COLOR=1', 'NPM_CONFIG_COLOR=always', 'GK_LOCK_COMMIT_AMEND=true']}
+    }
+  )));
 
   test('that a badge is not defined and coverage is not reported for a private project', () => assert.becomes(
     scaffold({projectType: 'JavaScript', projectRoot, vcs, visibility: 'Private'}),
@@ -56,6 +54,24 @@ suite('travis', () => {
       install: ['npm install', 'gem install travis'],
       before_script: ['npm run greenkeeper:update-lockfile', 'npm ls >/dev/null'],
       after_script: 'npm run greenkeeper:upload-lockfile',
+      env: {global: ['FORCE_COLOR=1', 'NPM_CONFIG_COLOR=always', 'GK_LOCK_COMMIT_AMEND=true']}
+    }
+  )));
+
+  test('that packages get deployed with semantic-release, but tag builds are excluded', () => assert.becomes(
+    scaffold({projectType: 'JavaScript', projectRoot, vcs, visibility: 'Private', packageType: 'Package'}),
+    {}
+  ).then(() => assert.calledWith(
+    yamlWriter.default,
+    `${projectRoot}/.travis.yml`,
+    {
+      language: 'node_js',
+      notifications: {email: false},
+      branches: {except: ["/^v\\d+\\.\\d+\\.\\d+$/"]},
+      install: ['npm install', 'gem install travis'],
+      before_script: ['npm run greenkeeper:update-lockfile', 'npm ls >/dev/null'],
+      after_script: 'npm run greenkeeper:upload-lockfile',
+      deploy: {provider: 'script', skip_cleanup: true, script: 'npx semantic-release'},
       env: {global: ['FORCE_COLOR=1', 'NPM_CONFIG_COLOR=always', 'GK_LOCK_COMMIT_AMEND=true']}
     }
   )));
