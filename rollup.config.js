35fe39061df8eb8949e1d84f3fd4754ae3442560
Matt Travi <programmer@travi.org>
2020-08-29 20:16:38 -0500

    build(rollup): configured the auto-external plugin
    so that dependencies are excluded from the bundle and expected to be provided when consumed

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,9 @@
 /* eslint import/no-extraneous-dependencies: ['error', {'devDependencies': true}] */
+import autoExternal from 'rollup-plugin-auto-external';
 
 export default {
   input: 'src/index.js',
-  external: ['util', 'write-yaml'],
+  plugins: [autoExternal()],
   output: [
     {file: 'lib/index.cjs.js', format: 'cjs', sourcemap: true},
     {file: 'lib/index.es.js', format: 'es', sourcemap: true}
